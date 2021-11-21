084bfc4a74786f12769cedd4c0157c4d7946575e
fischundfleisch <94743049+fischundfleisch@users.noreply.github.com>
2021-11-21 09:43:39 +0100

    Update timer.ino
    added heartbeat Line 25 -27 and global const HEARTBEAT_FREQ, 10 minutes at the moment

--- timer.ino
+++ timer.ino
@@ -1,5 +1,6 @@
 const unsigned long DURATION_ON = 15 * 60 * 60 * 1000; //globale Konstanten in CAPS
 const unsigned long DURATION_OFF  = 9 * 60 * 60 * 1000;
+const unsigned long HEARTBEAT_FREQ = 10*60*1000;
 const unsigned int LED_PIN = 5;
 
 unsigned long act_time_ = 0;
@@ -21,6 +22,10 @@ void loop() {
 
 unsigned long time_span = millis() - act_time_;
 
+  if (time_span > HEARTBEAT_FREQ){
+    Serial.println("!");
+  }
+  
   if (light_on_) {
     if (time_span > DURATION_ON) {
       digitalWrite(LED_PIN, LOW);
